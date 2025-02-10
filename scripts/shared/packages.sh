9941406c884c2ac730d1503046370f66d2b41aff
Tomek <bobrowicz.tomek@gmail.com>
2025-02-10 19:23:09 +0100

    remove ventoy

--- scripts/shared/packages.sh
+++ scripts/shared/packages.sh
@@ -10,6 +10,6 @@ wget https://download.opensuse.org/repositories/home:paul4us/Fedora_Rawhide/home
 wget https://download.opensuse.org/repositories/home:Alxhr0/Fedora_41/home:Alxhr0.repo -P /etc/yum.repos.d
 dnf5 -y copr enable peterwu/rendezvous
 
-dnf -y install bibata-cursor-themes papirus-icon-theme ventoy libappstream-glib virt-manager virt-install libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top nu code
+dnf -y install bibata-cursor-themes papirus-icon-theme libappstream-glib virt-manager virt-install libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top nu code
 
 systemctl enable podman.socket libvirtd
