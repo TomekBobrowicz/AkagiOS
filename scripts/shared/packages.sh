0f0b07cb90d80617be6bf68f940914ab7c96126f
Tomek <bobrowicz.tomek@gmail.com>
2025-02-10 23:16:39 +0100

    hyprland

--- scripts/shared/packages.sh
+++ scripts/shared/packages.sh
@@ -15,7 +15,7 @@ dnf5 -y copr enable tofik/nwg-shell
 dnf5 -y copr enable erikreider/SwayNotificationCenter
 dnf5 -y copr enable varlad/yazi
 
-dnf -y install bibata-cursor-themes papirus-icon-theme libappstream-glib virt-manager virt-install libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top nu code google-chrome-stable hyprland hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland kitty qt5-qtwayland qt6-qtwayland 
+dnf -y install bibata-cursor-themes papirus-icon-theme libappstream-glib virt-manager virt-install libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top nu code google-chrome-stable hyprland hyprpaper hyprlock hypridle hyprland-qtutils xdg-desktop-portal-hyprland kitty  
 dnf -y install cliphist nwg-look nwg-dock-hyprland SwayNotificationCenter yazi neovim btop grim slurp waybar rofi-wayland qt6ct nm-connection-editor network-manager-applet pavucontrol papirus-icon-theme-dark wlogoutfontawesome-6-free-fonts NetworkManager-tui brightnessctl tumbler   
 
 pip install hyprshade
