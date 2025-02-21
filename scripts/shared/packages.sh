c89f51c1eec47f2fedfcb44adc3bb6719df30c39
Tomek <bobrowicz.tomek@gmail.com>
2025-02-21 15:09:39 +0100

    Hyprland into plasma

--- scripts/shared/packages.sh
+++ scripts/shared/packages.sh
@@ -10,7 +10,13 @@ wget https://download.opensuse.org/repositories/home:paul4us/Fedora_Rawhide/home
 wget https://download.opensuse.org/repositories/home:Alxhr0/Fedora_41/home:Alxhr0.repo -P /etc/yum.repos.d
 
 dnf5 -y copr enable peterwu/rendezvous
+dnf5 -y copr enable solopasha/hyprland
+dnf5 -y copr enable tofik/nwg-shell
+dnf5 -y copr enable erikreider/SwayNotificationCenter
+dnf5 -y copr enable varlad/yazi
 
+dnf5 -y install bibata-cursor-themes papirus-icon-theme libappstream-glib virt-manager virt-install libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top nu code hyprland hyprpaper hyprlock hypridle hyprland-qtutils xdg-desktop-portal-hyprland kitty  
+dnf5 -y install pipx cliphist nwg-look nwg-dock-hyprland SwayNotificationCenter yazi neovim btop grim slurp waybar rofi-wayland qt6ct nm-connection-editor network-manager-applet pavucontrol papirus-icon-theme-dark wlogout fontawesome-6-free-fonts NetworkManager-tui brightnessctl tumbler
+dnf5 -y install hyprpicker eza xclip htop pinta blueman mozilla-fira-sans-fonts discord  
 
-dnf5 -y install bibata-cursor-themes papirus-icon-theme libappstream-glib gvfs-smb gvfs-fuse virt-manager virt-install libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top nu code neovim btop discord 
 systemctl enable podman.socket libvirtd
